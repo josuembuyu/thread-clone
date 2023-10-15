be994a017097848730cc7b10d7283fcd3950dede
josuembuyu6 <josuembuyu94@gmail.com>
2023-10-16 00:09:56 +0100

    Implement all the features

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,12 +1,10 @@
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
-import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
 
 import { redirect } from "next/navigation";
-import { profileTabs } from "@/constants";
-import Image from "next/image";
-import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import CommunityCard from "@/components/cards/CommunityCard";
 
 async function Page() {
   const user = await currentUser();
@@ -16,9 +14,35 @@ async function Page() {
 
   if (!userInfo?.onboarded) redirect("/onboarding");
 
+  const result = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
-      <h1 className="head-text mb-10">Communities</h1>
+      <h1 className="head-text mb-10">Search</h1>
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No communities</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 }
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -16,7 +17,7 @@ interface Props {
     name: string;
     image: string;
   } | null;
-  createAt: string;
+  createdAt: string;
   comments: {
     author: {
       image: string;
@@ -32,7 +33,7 @@ const ThreadCard = ({
   content,
   author,
   community,
-  createAt,
+  createdAt,
   comments,
   isComment,
 }: Props) => {
@@ -108,7 +109,27 @@ const ThreadCard = ({
             </div>
           </div>
         </div>
+        {/* TODO: DeleteThread */}
+        {/* TODO: Show comment logos */}
       </div>
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
+        >
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)} - {community.name} Community
+          </p>
+
+          <Image
+            src={community.image}
+            alt={community.name}
+            width={14}
+            height={14}
+            className="ml-1 rounded-full object-cover"
+          />
+        </Link>
+      )}
     </article>
   );
 };
