7380e013ede8f9886e940bd428b4aeb6382ff094
josuembuyu6 <josuembuyu94@gmail.com>
2023-10-23 17:43:49 +0100

    Adding all part of project

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,21 +1,27 @@
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
-import { redirect } from "next/navigation";
 import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-async function Page() {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchUsers({
     userId: user.id,
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
@@ -23,9 +29,11 @@ async function Page() {
     <section>
       <h1 className="head-text mb-10">Search</h1>
 
+      <Searchbar routeType="search" />
+
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
-          <p className="no-result">No users</p>
+          <p className="no-result">No Result</p>
         ) : (
           <>
             {result.users.map((person) => (
@@ -41,6 +49,12 @@ async function Page() {
           </>
         )}
       </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   );
 }
